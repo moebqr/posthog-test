0f0974e29e4e5a4b34a1d9db2939614b8dc74484
moebqr <71250489+moebqr@users.noreply.github.com>
2024-05-18 21:24:34 +0300

    added posthog snippet

--- posthog-test/src/index.js
+++ posthog-test/src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 
+posthog.init('phc_xNxPF5F16I9D4BQeSXqR0bFL0ly3YNKU8jZzFjjtUha', {
+  api_host: '"https://us.i.posthog.com"',
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
